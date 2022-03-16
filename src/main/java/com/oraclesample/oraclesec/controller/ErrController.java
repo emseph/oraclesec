82927debc1302814ae58598de3ffb05871d61dc0
John Paul Conrad Castillo <jpcon45@gmail.com>
2022-03-16 22:28:05 +0800

    summary required

--- src/main/java/com/oraclesample/oraclesec/controller/ErrController.java
+++ src/main/java/com/oraclesample/oraclesec/controller/ErrController.java
@@ -1,15 +1,15 @@
-//package com.oraclesample.oraclesec.controller;
+// package com.oraclesample.oraclesec.controller;
 //
-//import org.springframework.boot.web.servlet.error.ErrorController;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.boot.web.servlet.error.ErrorController;
+// import org.springframework.stereotype.Controller;
+// import org.springframework.web.bind.annotation.RequestMapping;
 //
-//@Controller
-//public class ErrController implements ErrorController  {
+// @Controller
+// public class ErrController implements ErrorController {
 //
-//    @RequestMapping("/error")
-//    public String handleError() {
-//        //do something like logging
-//        return "error";
-//    }
-//}
+// @RequestMapping("/error")
+// public String handleError() {
+// //do something like logging
+// return "error";
+// }
+// }
